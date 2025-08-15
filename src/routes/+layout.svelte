66fc8e4eb953b2c2ac58b0b14b2147eaffb0bb95
BrandonPilane <BrandonPilane@gmail.com>
2025-08-15 08:17:00 +0200

    chore: format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
-	
+
 	let { children } = $props();
 </script>
 
@@ -10,5 +10,5 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<Header/>
+<Header />
 {@render children?.()}
